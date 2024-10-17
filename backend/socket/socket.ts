190f4e3c60c849f2c3b823e3c57cd14a93d4b2e5
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-10-17 19:08:57 +0100

    Project completion

--- backend/socket/socket.ts
+++ backend/socket/socket.ts
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["https://chatbot-final-c7fd.onrender.com"],
         methods: ["GET", "POST"],
     },
 });
